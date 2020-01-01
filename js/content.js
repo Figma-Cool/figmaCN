bce6d4e40578ef20d2926a6778efcd1e60f5f383
yancymin <senseslofter@gmail.com>
2020-01-01 23:55:55 +0800

    index page, edit panel

--- js/content.js
+++ js/content.js
@@ -1,5 +1,3 @@
-
-
 document.body.addEventListener(
   'DOMSubtreeModified',
   () => {
@@ -9,7 +7,17 @@ document.body.addEventListener(
     var panelTitle = document.querySelectorAll('div[class*="panelTitle"]')
     var panelTitleText = document.querySelectorAll('div[class*="panelTitleText"]')
     var panelTab = document.querySelectorAll('div[class*="panel--tab"]')
-    
+    var selectOptionText = document.querySelectorAll('span[class*="select--optionText"]')
+    var selectOptionInputText = document.querySelectorAll('span[class*="select--inputText"]')
+    var imageAdjustLabel = document.querySelectorAll('div[class*="image_settings_modal--colorAdjustLabel"]')
+    var checkboxLabel = document.querySelectorAll('label[class*="checkboxLabel"]')
+    var toolBarDropDown = document.querySelectorAll('a[class*="dropdown"]')
+    var inputPlaceholder = document.querySelectorAll('input')
+    var allLabel = document.querySelectorAll('label')
+    var spanSidebar = document.querySelectorAll('span[class*="sidebar"]')
+    var divSidebar = document.querySelectorAll('div[class*="sidebar"]')
+    var spanToolBar = document.querySelectorAll('span[class*="tool_bar"]')
+
     function replaceTextNodes(node) {
       node.forEach(function (el) {
         for (var i = 0; i < data.length; i++) {
@@ -40,6 +48,38 @@ document.body.addEventListener(
             el.textContent = panelTabs[i][1]
           }
         }
+
+        for (var i = 0; i < selectOptionTextData.length; i++) {
+          if (el.textContent === selectOptionTextData[i][0]) {
+            el.innerHTML = selectOptionTextData[i][1]
+          }
+        }
+        for (var i = 0; i < checkboxLabelData.length; i++) {
+          if (el.textContent === checkboxLabelData[i][0]) {
+            el.innerHTML = checkboxLabelData[i][1]
+          }
+        }
+        for (var i = 0; i < toolBarDropDownData.length; i++) {
+          if (el.textContent === toolBarDropDownData[i][0]) {
+            el.innerHTML = toolBarDropDownData[i][1]
+          }
+        }
+        for (var i = 0; i < inputPlaceholderData.length; i++) {
+          if (el.placeholder === inputPlaceholderData[i][0]) {
+            el.placeholder = inputPlaceholderData[i][1]
+          }
+        }
+        for (var i = 0; i < allLabelData.length; i++) {
+          if (el.innerHTML === allLabelData[i][0]) {
+            el.innerHTML = allLabelData[i][1]
+          }
+        }
+        for (var i = 0; i < spanSidebarToolbarData.length; i++) {
+          if (el.innerHTML === spanSidebarToolbarData[i][0]) {
+            el.innerHTML = spanSidebarToolbarData[i][1]
+          }
+        }
+
       })
     }
 
@@ -50,6 +90,16 @@ document.body.addEventListener(
     replaceTextNodes(menuTitlesSpan)
     replaceTextNodes(menuTitles)
     replaceTextNodes(actionOption)
+    replaceTextNodes(selectOptionText)
+    replaceTextNodes(selectOptionInputText)
+    replaceTextNodes(imageAdjustLabel)
+    replaceTextNodes(checkboxLabel)
+    replaceTextNodes(toolBarDropDown)
+    replaceTextNodes(inputPlaceholder)
+    replaceTextNodes(allLabel)
+    replaceTextNodes(spanSidebar)
+    replaceTextNodes(spanToolBar)
+    replaceTextNodes(divSidebar)
   },
   false
 )
