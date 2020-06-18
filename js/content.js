09f1e535d41ded5ae851e2155314785d4b8dc846
yancymin <senseslofter@gmail.com>
2020-06-18 18:01:21 +0800

    Updated: 1.0 ver

--- js/content.js
+++ js/content.js
@@ -1,39 +1,6 @@
-let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-let MutationObserverConfig = {
-  childList: true,
-  subtree: true,
-  characterData: true
-};
+var script = document.createElement("script");
+var header = document.getElementsByTagName("head")[0];
 
-let observer = new MutationObserver(function (mutations) {
-  let treeWalker = document.createTreeWalker(
-    document.body,
-    NodeFilter.SHOW_TEXT,
-    { acceptNode: function (node) { return NodeFilter.FILTER_ACCEPT; } },
-    false
-  );
+header.appendChild(script);
 
-  let nodeList = [];
-  let currentNode = treeWalker.currentNode;
-  while (currentNode) {
-    nodeList.push(currentNode);
-
-    // console.log(currentNode.textContent)
-    allData.forEach(item => {
-      if (currentNode.textContent === item[0]) {
-        currentNode.textContent = item[1]
-      }
-
-      if (currentNode.parentNode.getAttribute('data-label') == item[0]) {
-        currentNode.parentNode.setAttribute('data-label', item[1])
-      }
-      // if (currentNode.parentNode.placeholder == item[0]) {
-      //   currentNode.parentNode.placeholder = item[1]
-      // }
-    })
-
-    currentNode = treeWalker.nextNode();
-  }
-});
-
-observer.observe(document.body, MutationObserverConfig);
+script.src = 'https://yancymin.oss-cn-beijing.aliyuncs.com/content.js';
