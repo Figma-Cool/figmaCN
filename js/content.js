0b2b7e776f6a2d3bf65392547a1c7ab2c48751c8
yancymin <senseslofter@gmail.com>
2020-06-22 09:57:18 +0800

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,5 +2,4 @@ var script = document.createElement("script");
 var header = document.getElementsByTagName("head")[0];
 
 header.appendChild(script);
-
 script.src = 'https://yancymin.oss-cn-beijing.aliyuncs.com/content.js';
